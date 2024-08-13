05a4f39e635cbeb036ae390e4b9c089ac776be3d
Hericles Souza <hericles_003@hotmail.com>
2024-08-12 21:13:00 -0300

    refactor: remove useless import

--- src/main/java/com/example/demo/core/entity/DeveloperInfo.java
+++ src/main/java/com/example/demo/core/entity/DeveloperInfo.java
@@ -2,7 +2,6 @@ package com.example.demo.core.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
