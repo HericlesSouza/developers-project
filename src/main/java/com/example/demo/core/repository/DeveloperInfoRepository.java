aeb9d276414dc15b86e8cb602a049ce43aff77f5
Hericles Souza <hericles_003@hotmail.com>
2024-08-12 21:13:30 -0300

    feat: create repository project

--- src/main/java/com/example/demo/core/repository/DeveloperInfoRepository.java
+++ src/main/java/com/example/demo/core/repository/DeveloperInfoRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.core.repository;
 
 import com.example.demo.core.entity.DeveloperInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeveloperInfoRepository extends JpaRepository<DeveloperInfo, Long> {
 }
